83a7761eb26249e2b0ce1edcdce056e5ee31c68d
IssaZeidan <Issa.Zeidan94@gmail.com>
2023-03-27 14:33:14 +0300

    updated the readme file and added a text.js file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Problem-solving
+
+In this project I solved three problems using Java script programming language as the questions are :
+
+* Write a program to print Fibonacci for a given number.
+The Fibonacci series is a series of numbers in which each is the sum of the two preceding ones, usually starting with 0 and 1. 
+
+* Write a program that Calculates the sum of elements in an array
+
+* Write a program that Reverses a string 
---
+++ test.js
@@ -0,0 +1,35 @@
+// Q1:
+
+// function fib(n) {
+//   return n <= 1 ? n : fib(n - 1) + fib( n - 2);
+// }
+// console.log(fib(5));
+// console.log(fib(4));
+// console.log(fib(3));
+// console.log(fib(2));
+// console.log(fib(1));
+// console.log(fib(0));
+
+// Q2:
+
+// let sum= function(array){
+//     return array.length === 0 ? 0 : array [0] + sum (array.slice(1))
+// }
+
+// console.log(sum([1,2,3,4,5]));
+
+//Q3:
+
+//  function reverse (str) {
+//  if (str === "")
+//  {
+//  return "";
+// }
+//  else
+//  {
+//  return reverse(str.substr(1)) + str.charAt(0);
+//  }
+//  }
+//  console.log(reverse("hello"));
+
+
